3dffb20d43f26278f3360825ad9d1b567f6bd148
Abdul Rehman <ar618211@gmail.com>
2025-12-06 21:52:58 +0500

    day 5 a done

--- day5/a.go
+++ day5/a.go
@@ -1,5 +1,51 @@
 package day5
 
+import (
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	S int
+	E int
+}
+
 func PartA(data string) int {
-	return 0
+	raw := strings.Split(data, "\n\n")
+	if len(raw) < 2 {
+		panic("wrong puzzle input")
+	}
+	rangesRaw := strings.Split(raw[0], "\n")
+	ids := strings.Split(raw[1], "\n")
+
+	ranges := make([]Range, len(rangesRaw))
+
+	for i, r := range rangesRaw {
+		raw := strings.Split(r, "-")
+		if len(raw) < 2 {
+			panic("wrong puzzle input")
+		}
+		s, _ := strconv.Atoi(raw[0])
+		e, _ := strconv.Atoi(raw[1])
+
+		ranges[i] = Range{
+			S: s,
+			E: e,
+		}
+	}
+
+	count := 0
+
+	for _, idStr := range ids {
+		id, _ := strconv.Atoi(idStr)
+
+		for _, r := range ranges {
+			if id >= r.S && id <= r.E {
+				count++
+				break
+			}
+		}
+	}
+
+	return count
 }
--- day5/day5_test.go
+++ day5/day5_test.go
@@ -5,18 +5,27 @@ import (
 )
 
 const (
-	data = ""
+	data = `3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32`
 )
 
 func TestPartA(t *testing.T) {
-	expected := 0
+	expected := 3
 	result := PartA(data)
 
 	if expected != result {
 		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
 	}
 }
-
 
 func TestPartB(t *testing.T) {
 	expected := 0
