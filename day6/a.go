6a61d150d78406e7b0dc701b1dea115333cf1860
Abdul Rehman <ar618211@gmail.com>
2025-12-07 01:04:13 +0500

    day 6 a done

--- day6/a.go
+++ day6/a.go
@@ -1,5 +1,68 @@
 package day6
 
+import (
+	"advent-of-code-2025/utils"
+)
+
 func PartA(data string) int {
-	return 0
+	lines := utils.GetLines(data)
+
+	nums := [][]int{}
+	exps := []bool{} // multiply=true
+
+	for i, s := range lines {
+		if i == len(lines)-1 {
+			for _, ch := range s {
+				switch ch {
+				case '+':
+					exps = append(exps, false)
+				case '*':
+					exps = append(exps, true)
+				}
+			}
+			continue
+		}
+		row := []int{}
+
+		x := 0
+
+		for i, ch := range s {
+			if ch == ' ' {
+				if x != 0 {
+					row = append(row, x)
+					x = 0
+				}
+				continue
+			}
+
+			x = (x * 10) + int(ch-'0')
+
+			if i == len(s)-1 {
+				row = append(row, x)
+			}
+		}
+
+		nums = append(nums, row)
+	}
+
+	limit := len(nums[0])
+	ans := 0
+
+	for i := range limit {
+		a := 0
+		if exps[i] {
+			a = 1
+		}
+		for _, num := range nums {
+			if exps[i] {
+				a *= num[i]
+			} else {
+				a += num[i]
+			}
+		}
+
+		ans += a
+	}
+
+	return ans
 }
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -5,18 +5,20 @@ import (
 )
 
 const (
-	data = ""
+	data = `123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  `
 )
 
 func TestPartA(t *testing.T) {
-	expected := 0
+	expected := 4277556
 	result := PartA(data)
 
 	if expected != result {
 		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
 	}
 }
-
 
 func TestPartB(t *testing.T) {
 	expected := 0
