1cb18158ab00a28cd881c9c352710a973703ce61
Abdul Rehman <ar618211@gmail.com>
2025-12-07 01:29:51 +0500

    day 6 b done

--- day6/b.go
+++ day6/b.go
@@ -1,5 +1,58 @@
 package day6
 
+import (
+	"advent-of-code-2025/utils"
+)
+
+func blah(nums []int, multiply bool) int {
+	a := 0
+	if multiply {
+		a = 1
+	}
+	for _, num := range nums {
+		if multiply {
+			a *= num
+		} else {
+			a += num
+		}
+	}
+	return a
+}
+
 func PartB(data string) int {
-	return 0
+	lines := utils.GetLines(data)
+
+	max_rows := len(lines)
+	max_cols := len(lines[0])
+
+	ans := 0
+
+	nums := []int{}
+	multiply := false
+	for c := range max_cols {
+		x := 0
+		for r := range max_rows {
+			ch := lines[r][c]
+			if ch == ' ' {
+				continue
+			} else if ch == '+' {
+				multiply = false
+			} else if ch == '*' {
+				multiply = true
+			} else {
+				x = (x * 10) + int(ch-'0')
+			}
+		}
+		if x == 0 {
+			// empty
+			ans += blah(nums, multiply)
+			nums = []int{}
+		} else {
+			nums = append(nums, x)
+		}
+	}
+
+	ans += blah(nums, multiply)
+
+	return ans
 }
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -21,7 +21,7 @@ func TestPartA(t *testing.T) {
 }
 
 func TestPartB(t *testing.T) {
-	expected := 0
+	expected := 3263827
 	result := PartB(data)
 
 	if expected != result {
