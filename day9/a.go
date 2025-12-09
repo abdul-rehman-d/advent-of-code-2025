0b5cf73178be5bfe2accbf2ca6d89fdc6bb4ff83
Abdul Rehman <ar618211@gmail.com>
2025-12-09 15:32:39 +0500

    day 9 a done

--- day9/a.go
+++ day9/a.go
@@ -1,5 +1,50 @@
 package day9
 
+import (
+	"advent-of-code-2025/utils"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+type Coord = utils.Coord
+
+func area(a, b Coord) int {
+	l := math.Abs(float64(a.R)-float64(b.R)) + 1
+	w := math.Abs(float64(a.C)-float64(b.C)) + 1
+
+	return int(l * w)
+}
+
 func PartA(data string) int {
-	return 0
+	lines := utils.GetLines(data)
+	coords := make([]Coord, len(lines))
+
+	for i, line := range lines {
+		a := strings.Split(line, ",")
+		if len(a) < 2 {
+			panic(fmt.Sprintf("less than 2 coordinates at ln %d", i))
+		}
+		x, _ := strconv.Atoi(a[0])
+		y, _ := strconv.Atoi(a[1])
+
+		coords[i] = Coord{R: x, C: y}
+	}
+
+	largest := 0
+
+	for i, ci := range coords {
+		for j, cj := range coords {
+			if i == j {
+				continue
+			}
+			a := area(ci, cj)
+			if largest < a {
+				largest = a
+			}
+		}
+	}
+
+	return largest
 }
--- day9/day9_test.go
+++ day9/day9_test.go
@@ -5,18 +5,24 @@ import (
 )
 
 const (
-	data = ""
+	data = `7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3`
 )
 
 func TestPartA(t *testing.T) {
-	expected := 0
+	expected := 50
 	result := PartA(data)
 
 	if expected != result {
 		t.Fatalf("\nExpected = %d\nResult = %d\n", expected, result)
 	}
 }
-
 
 func TestPartB(t *testing.T) {
 	expected := 0
